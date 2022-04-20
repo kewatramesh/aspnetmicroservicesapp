a353ea93445b4decce444dd3d76cb1d18952233e
RAMESH <kewat.ramesh.harishankar@gmail.com>
2022-04-20 01:40:23 -0700

    catalog api with docker done

---
+++ src/Services/Catalog/CatalogAPI/Data/CatalogContext.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatalogAPI.Entities;
+
+namespace CatalogAPI.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
+        }
+
+        public IMongoCollection<Product> Products { get; }
+    }
+
+
+}
